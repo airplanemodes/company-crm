edbf71f7602a63aa8765998cc172059b02b356d3
airplanemodes <74984133+airplanemodes@users.noreply.github.com>
2021-09-01 20:24:09 +0300

    improvement of 404

--- src/app/components/pagenotfound/pagenotfound.component.ts
+++ src/app/components/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -7,17 +8,36 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div class="container-fluid mx-auto text-center">
     <p class="display-3 pt-5">404 PAGE NOT FOUND <i class="fa fa-exclamation" aria-hidden="true"></i></p>
-    <a class="text-success text-decoration-none fs-4" routerLink="/">Back to login</a>
+    <!-- Showing different links accordingly to the login information  -->
+    <div *ngIf="logged; else elseBlock">
+      <a class="text-success text-decoration-none fs-4" routerLink="/admin">Back to the system</a>
+    </div>
+    <ng-template #elseBlock>
+      <a class="text-success text-decoration-none fs-4" routerLink="/">Back to login</a>
+    </ng-template>
   </div> 
   `,
   
   styleUrls: ['./pagenotfound.component.css']
 })
+
+
+
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  logged = false;
 
+  constructor(private authCheck:AngularFireAuth) { }
+
+  // Getting a login information for that ngIf above
   ngOnInit(): void {
-  }
+    this.authCheck.authState.subscribe((user:any) => {
+      if (user) {
+        this.logged = true;
+      } else {
+        this.logged = false;
+      }
+    });
+  };
 
 };
